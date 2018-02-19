d877077f562205e35686c6d87c1899b9f3c12aa8
Davide Polonio <poloniodavide@gmail.com>
2018-02-19 12:39:39 +0100

    Add BT Check

--- app/src/main/java/com/codernauti/gamebank/MainActivity.java
+++ app/src/main/java/com/codernauti/gamebank/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.codernauti.gamebank.lobby.LobbyActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(this, DashboardActivity.class));
+        startActivity(new Intent(this, LobbyActivity.class));
     }
 }
--- app/src/main/java/com/codernauti/gamebank/lobby/LobbyActivity.java
+++ app/src/main/java/com/codernauti/gamebank/lobby/LobbyActivity.java
@@ -1,15 +1,22 @@
 package com.codernauti.gamebank.lobby;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.codernauti.gamebank.R;
 
-public class lobbyActivity extends AppCompatActivity {
+public class LobbyActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 3;
+
+    private BluetoothAdapter mBluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,25 @@ public class lobbyActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            this.finish();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            // Otherwise, setup the chat session
+        }
     }
 
 }
