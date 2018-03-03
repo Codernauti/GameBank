6441f0a3880aa583965aa8cc57b1b22de5215ac8
EduBic <bic.studio@yahoo.it>
2018-03-03 19:47:52 +0100

    Close server bt connection

--- app/src/main/java/com/codernauti/gamebank/bluetooth/BTHostConnection.java
+++ app/src/main/java/com/codernauti/gamebank/bluetooth/BTHostConnection.java
@@ -73,43 +73,6 @@ public class BTHostConnection extends BTConnection {
                             mLocalBroadcastManager.sendBroadcast(intentJoin);
                         }
 
-
-                       /* Intent intentConn = new Intent(Event.Network.CONN_ESTABLISHED);
-                        mLocalBroadcastManager.sendBroadcast(intentConn);*/
-
-                           /*
-                                if (Event.Game.MEMBER_JOINED.equals(btBundle.getBluetoothAction())) {
-                                    // who sent this packet
-                                    UUID clientUuid = btBundle.getUuid();
-
-                                    // update app network layer
-                                    addConnection(clientUuid, clientSocket);
-                                    startListeningRunnable(clientUuid);
-
-                                    // broadcast
-                                    Intent intent = BTBundle.makeIntentFrom(btBundle);
-                                    mLocalBroadcastManager.sendBroadcast(intent);
-                                }*/
-
-                                /*if (Event.Network.INIT_INFORMATION.equals(btBundle.getBluetoothAction())) {
-
-                                    UUID client = (UUID) btBundle.get(UUID.class.getName());
-
-                                    addConnection(client, clientSocket);
-                                    startListeningRunnable(client);
-
-                                    // update Ui
-                                    String key = RoomPlayer.class.getName();
-                                    Intent connection = BTBundle.makeIntentFrom(
-                                            new BTBundle(Event.Game.MEMBER_JOINED).append(
-                                                    btBundle.get(key))
-                                    );
-
-                                    mLocalBroadcastManager.sendBroadcast(connection);
-                                }*/
-
-                        Log.d(TAG, "Ended waiting for new connections");
-
                     } catch (IOException e) {
                         Log.e(TAG, "Socket closed, connection accept abort.\nAutomatically retry.");
                         i--;
@@ -118,6 +81,13 @@ public class BTHostConnection extends BTConnection {
                         Log.e(TAG, "Init data from client is not a UUID");
                         e.printStackTrace();
                     }
+                }
+
+                Log.d(TAG, "Ended waiting for new connections");
+                try {
+                    mBtServerSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         });
