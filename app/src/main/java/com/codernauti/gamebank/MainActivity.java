84e5bf7707c62fc3eecd1571605bbf8e0ad090e4
Davide Polonio <poloniodavide@gmail.com>
2018-03-06 13:56:03 +0100

    Add settings and other stuff

--- app/src/main/java/com/codernauti/gamebank/MainActivity.java
+++ app/src/main/java/com/codernauti/gamebank/MainActivity.java
@@ -4,13 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.support.v7.widget.Toolbar;
 
 import com.codernauti.gamebank.lobby.client.LobbyActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @BindView(R.id.main_activity_toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +25,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
     }
 
     @OnClick(R.id.lobby_button)
     void onClickButton() {
 
         startActivity(new Intent(this, LobbyActivity.class));
+    }
+
+    @OnClick(R.id.load_match_button)
+    void onLoadingMatchClickButton() {
+
+        Log.e(TAG, "Missing implementation for onLoadingMatchClickButton");
+    }
+
+    @OnClick(R.id.settings_button)
+    void onSettingsClickButton() {
+
+        startActivity(new Intent(this, SettingsActivity.class));
     }
 }
---
+++ app/src/main/java/com/codernauti/gamebank/SettingsActivity.java
@@ -0,0 +1,27 @@
+package com.codernauti.gamebank;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @BindView(R.id.settings_toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
