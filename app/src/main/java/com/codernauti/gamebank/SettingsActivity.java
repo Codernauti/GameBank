65803a6957f0247d2c3214ee8cc37d83d4213199
EduBic <bic.studio@yahoo.it>
2018-03-07 10:11:21 +0100

    Add support to home button in EditTextActivity

--- app/src/main/java/com/codernauti/gamebank/SettingsActivity.java
+++ app/src/main/java/com/codernauti/gamebank/SettingsActivity.java
@@ -8,6 +8,8 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -19,6 +21,10 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private static final String TAG = "SettingsActivity";
+
+    private static final int NICKNAME_RESULT = 10;
 
     @BindView(R.id.settings_toolbar)
     Toolbar toolbar;
@@ -35,16 +41,36 @@ public class SettingsActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NICKNAME_RESULT && resultCode == RESULT_OK) {
+            Log.d(TAG, "From EditTextActivity: " + data.getExtras().get(EditTextActivity.TEXT_RESULT));
+
+        } else if (resultCode == RESULT_CANCELED) {
+            Log.d(TAG, "Result Canceled");
+        }
     }
 
     @OnClick(R.id.change_name_button)
     public void onChangeNameClickedButton() {
 
         Intent intent = new Intent(this, EditTextActivity.class);
-        intent.putExtra(EditTextActivity.TOOLBAR_TITLE, "Change player name");
-        intent.putExtra(EditTextActivity.EDIT_FIELD_NAME, "Player name");
-        intent.putExtra(EditTextActivity.DESCRIPTION, "Bla bla bla bla");
 
-        startActivity(intent);
+        Bundle bundle = new Bundle();
+        bundle.putString(EditTextActivity.TOOLBAR_TITLE, "Change player name");
+        bundle.putString(EditTextActivity.EDIT_FIELD_NAME, "Player name");
+        bundle.putString(EditTextActivity.DESCRIPTION, "Bla bla bla bla");
+
+        intent.putExtras(bundle);
+
+        //startActivity(intent);
+        startActivityForResult(intent, NICKNAME_RESULT);
     }
+
 }
--- app/src/main/java/com/codernauti/gamebank/util/EditTextActivity.java
+++ app/src/main/java/com/codernauti/gamebank/util/EditTextActivity.java
@@ -1,24 +1,32 @@
 package com.codernauti.gamebank.util;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.codernauti.gamebank.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class EditTextActivity extends AppCompatActivity {
+
+    private static final String TAG = "EditTextActivity";
 
     public static final String TOOLBAR_TITLE = "toolbar_name";
     public static final String EDIT_FIELD_NAME = "edit_field_name";
     public static final String DESCRIPTION = "description";
-    private static final String TAG = "EditTextActivity";
+
+    public static final String TEXT_RESULT = "text_result";
 
     @BindView(R.id.edit_toolbar)
     Toolbar mToolbar;
@@ -59,6 +67,37 @@ public class EditTextActivity extends AppCompatActivity {
         } else {
             Log.d(TAG, "Intent data is null");
         }
+    }
+
+    @OnClick(R.id.edit_save_btn)
+    public void saveText() {
+        String result = mInput.getText().toString();
+
+        if (result.isEmpty()) {
+
+            Toast.makeText(this, getString(R.string.warning_edit_input),
+                    Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra(TEXT_RESULT, result);
+
+            setResult(RESULT_OK, returnIntent);
+            finish();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
