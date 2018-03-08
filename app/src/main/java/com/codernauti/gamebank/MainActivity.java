014908e04a452f6f175240485eccc9305d621cac
Davide Polonio <poloniodavide@gmail.com>
2018-03-08 11:53:43 +0100

    Add emoji rain

--- app/src/main/java/com/codernauti/gamebank/MainActivity.java
+++ app/src/main/java/com/codernauti/gamebank/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.support.v7.widget.Toolbar;
 
 import com.codernauti.gamebank.lobby.client.LobbyActivity;
+import com.luolc.emojirain.EmojiRainLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.main_activity_toolbar)
     Toolbar toolbar;
 
+    @BindView(R.id.group_emoji_container)
+    EmojiRainLayout mEmojiRainLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +31,32 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
 
+        mEmojiRainLayout.addEmoji(getDrawable(R.drawable.banknote_with_dollar_sign));
+        mEmojiRainLayout.addEmoji(getDrawable(R.drawable.banknote_with_euro_sign));
+        mEmojiRainLayout.addEmoji(getDrawable(R.drawable.banknote_with_yen_sign));
+        mEmojiRainLayout.addEmoji(getDrawable(R.drawable.banknote_with_pound_sign));
+        mEmojiRainLayout.addEmoji(getDrawable(R.drawable.money_bag));
+        mEmojiRainLayout.addEmoji(getDrawable(R.drawable.money_mouth_face));
+        mEmojiRainLayout.addEmoji(getDrawable(R.drawable.money_with_wings));
+        mEmojiRainLayout.addEmoji(getDrawable(R.drawable.bank));
+        mEmojiRainLayout.addEmoji(getDrawable(R.drawable.credit_card));
+        mEmojiRainLayout.addEmoji(getDrawable(R.drawable.gem_stone));
+
+        mEmojiRainLayout.setPer(3);
+        mEmojiRainLayout.setDuration(Integer.MAX_VALUE);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mEmojiRainLayout.startDropping();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mEmojiRainLayout.stopDropping();
     }
 
     @OnClick(R.id.lobby_button)
