b854f98ba8f84c2e0228d8c002ce0b1d433de2d9
EduBic <bic.studio@yahoo.it>
2018-05-11 14:46:39 +0200

    Restore random picture generator

--- app/src/main/java/com/codernauti/gamebank/util/generators/ProfilePicGenerator.java
+++ app/src/main/java/com/codernauti/gamebank/util/generators/ProfilePicGenerator.java
@@ -24,21 +24,21 @@ public class ProfilePicGenerator implements RandomContentGenerator {
         this.r = new Random();
 
 
-        //mDefaultPictures.add(R.drawable.default_profile_pic_1);
+        mDefaultPictures.add(R.drawable.default_profile_pic_1);
         mDefaultPictures.add(R.drawable.default_profile_pic_2);
-        /*mDefaultPictures.add(R.drawable.default_profile_pic_3);
+        mDefaultPictures.add(R.drawable.default_profile_pic_3);
         mDefaultPictures.add(R.drawable.default_profile_cat_1);
         mDefaultPictures.add(R.drawable.default_profile_cat_2);
         mDefaultPictures.add(R.drawable.default_profile_cat_3);
         mDefaultPictures.add(R.drawable.default_profile_dog_1);
         mDefaultPictures.add(R.drawable.default_profile_dog_2);
-        mDefaultPictures.add(R.drawable.default_profile_dog_3);*/
+        mDefaultPictures.add(R.drawable.default_profile_dog_3);
     }
 
     @NonNull
     @Override
     public Integer generateRandomContent() {
 
-        return mDefaultPictures.get(0/*r.nextInt(mDefaultPictures.size() -1 )*/);
+        return mDefaultPictures.get(r.nextInt(mDefaultPictures.size()));
     }
 }
